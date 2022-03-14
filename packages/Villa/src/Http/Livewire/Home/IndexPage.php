c2a0a750c5d2057cb935d03606ae4884a705551b
payamkamarzarin <payamkamarzarin@yahoo.com>
2022-03-14 17:26:25 +0330

    Write home and user side structure

---
+++ packages/Villa/src/Http/Livewire/Home/IndexPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Packages\Villa\src\Http\Livewire\Home;
+
+use Livewire\Component;
+
+class IndexPage extends Component
+{
+     public function render()
+     {
+         return view('Villa::Livewire.Home.indexPage');
+     }
+}
---
+++ packages/Villa/src/Http/Livewire/Home/InfoPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Packages\Villa\src\Http\Livewire\Home;
+
+use Livewire\Component;
+
+class InfoPage extends Component
+{
+    public function render()
+    {
+        return view('');
+    }
+}
