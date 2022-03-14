d739b5edcdfbc1f662357ac15e1583ed03732cb6
iman <iman.hp.dev5294@gmail.com>
2022-03-14 17:25:20 +0330

    price management edited villa

--- packages/Villa/src/Http/Livewire/Admin/PriceManagement.php
+++ packages/Villa/src/Http/Livewire/Admin/PriceManagement.php
@@ -5,46 +5,61 @@ namespace Packages\Villa\src\Http\Livewire\Admin;
 
 use Livewire\Component;
 use Packages\Villa\src\Models\Residence;
+use phpDocumentor\Reflection\Types\Integer;
 
 class PriceManagement extends Component
 {
     public Residence $residence;
+    public int|null $currentMonth = null;
+    public int|null $currentYear = null;
+
+    public $months = null;
 
     public function mount()
     {
+        $this->months = collect([
+            array('id' => 1 , 'text' => 'فروردین'),
+            array('id' => 12 , 'text' => 'اسفند'),
+        ]);
     }
 
     public function render()
     {
-     $calenderData =$this->getCalender();
-        return view('Villa::Livewire.Admin.PriceManagementPage', compact('calenderData'));
+        $calenderData = $this->getCalender();
+        $this->currentMonth = $calenderData->month;
+        $this->currentYear = $calenderData->year;
+        return view('Villa::Livewire.Admin.PriceManagmentPage', compact('calenderData'));
     }
 
     public function getCalender()
     {
         $req = [
-            'year' => null ,
-            'month' => null,
-            'minDate' => null ,
-            'maxDate' => null ,
+            'year' => $this->currentYear,
+            'month' => $this->currentMonth,
+            'minDate' => jdate()->format('Y-m-d'),
+            'maxDate' => null,
             'format ' => 15,
-            'data' => [
-        [
-            'date'=> '1400-10-12',
-            'items'=> [
-            'description'=> 'test1'
-            ]
-        ],
-        [
-            'date'=> '1400-10-12',
-            'items'=> [
-            'description'=> "test2"
-            ]
-        ]
-    ],
-
-
+            'data' => []
         ];
+
         return apiService()->getCalender($req);
+    }
+
+    public function previousMonth()
+    {
+        $this->currentMonth = $this->currentMonth === 1 ? 12 : $this->currentMonth - 1;
+        $this->currentYear = $this->currentYear - 1;
+
+    }
+
+    public function nextMonth()
+    {
+        if ($this->currentMonth === 12) {
+            $this->currentYear = $this->currentYear + 1;
+            $this->currentMonth = 1;
+        } else {
+            $this->currentMonth = $this->currentMonth + 1;
+
+        }
     }
 }
