abb3f80757fec6884059777cbac50f953651cc06
imanhp <iman.hp.dev@gmail.com>
2022-05-07 15:03:33 +0430

    header chnaged

--- resources/views/livewire/home/sections/headers.blade.php
+++ resources/views/livewire/home/sections/headers.blade.php
@@ -1,5 +1,5 @@
 <div>
-    @if(str_starts_with($route , 'info') || str_starts_with($route , 'list'))
+    @if(!str_starts_with($route , 'index'))
         <x-parnas.layouts.home-section.headerTwo />
     @else
         <x-parnas.layouts.home-section.header-desktop />
