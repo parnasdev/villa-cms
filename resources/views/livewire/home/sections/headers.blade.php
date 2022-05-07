8d8bb19a4ab21229d7a8b4a49e2ae455c1262005
imanhp <iman.hp.dev@gmail.com>
2022-05-07 15:37:56 +0430

    header and auth edited

--- resources/views/livewire/home/sections/headers.blade.php
+++ resources/views/livewire/home/sections/headers.blade.php
@@ -1,5 +1,5 @@
 <div>
-    @if(!str_starts_with($route , 'index'))
+    @if(! str_starts_with($route , 'index'))
         <x-parnas.layouts.home-section.headerTwo />
     @else
         <x-parnas.layouts.home-section.header-desktop />
