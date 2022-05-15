3c25c79a6273a9835a10b6911906ee1f9a784dfa
imanhp <iman.hp.dev@gmail.com>
2022-05-15 14:02:19 +0430

    date picker

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -3,6 +3,7 @@ import Bootstrap from './files/bootstrap.min'
 import Swal from 'sweetalert2'
 import Alpine from 'alpinejs'
 import IMask from 'imask';
+window.moment = require('jalali-moment')
 
 window.Alpine = Alpine
 window.Bootstrap = Bootstrap
